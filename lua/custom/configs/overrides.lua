c9b193cb17a7d2a7ff3fccbe0f20e1b541241f91
afrid18 <afridhussain.d@gmail.com>
2023-08-06 19:27:16 +0530

    treesitter: add incremental scope selection

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -21,6 +21,15 @@ M.treesitter = {
     --   "python"
     -- },
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      -- init_selection = "<CR>",
+      -- scope_incremental = "<CR>",
+      node_incremental = "<TAB>",
+      node_decremental = "<S-TAB>",
+    },
+  },
 }
 
 M.mason = {
