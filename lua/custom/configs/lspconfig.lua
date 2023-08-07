6ef921fa01c61d04b447b50be330bd2f0591a6bd
afrid18 <afridhussain.d@gmail.com>
2023-08-07 21:38:32 +0530

    format lua code in custom folder

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,15 +1,15 @@
-local config = require("plugins.configs.lspconfig")
+local config = require "plugins.configs.lspconfig"
 local on_attach = config.on_attach
 local capabilities = config.capabilities
 
-local lspconfig = require("lspconfig")
+local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
 
 -- Go LSP configuration
-lspconfig.gopls.setup ({
+lspconfig.gopls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  cmd = {"gopls"},
+  cmd = { "gopls" },
   filetypes = { "go", "gomod", "gowork", "gotmpl" },
   root_dir = util.root_pattern("go.work", "go.mod", ".git"),
   settings = {
@@ -21,19 +21,16 @@ lspconfig.gopls.setup ({
       },
     },
   },
-})
-
+}
 
 -- python LSP configuration
-lspconfig.pyright.setup ({
+lspconfig.pyright.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = {"python"},
-})
+  filetypes = { "python" },
+}
 
-
-
--- This is a general table to add default lsp, below is a for loop 
+-- This is a general table to add default lsp, below is a for loop
 -- that configures default lsp for all the strings in servers table
 local servers = { "html", "cssls", "tsserver", "clangd", "marksman" }
 
