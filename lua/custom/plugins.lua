4bb388cbc51ff7468ca157d867ff85ceefe4714e
afrid18 <afridhussain.d@gmail.com>
2023-08-09 20:11:47 +0530

    nvim: add neorg for orgmode support

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -60,6 +60,29 @@ local plugins = {
       vim.cmd [[silent! GoInstallDeps]]
     end,
   },
+
+  -- Neorg for Orgmode
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
 }
 
 return plugins
